d586e36df6130eef8dfb58b19fc4cf7deca288ca
Javier Povedano <javier@rti.com>
2019-01-05 11:56:05 +0100

    add gtest integration

--- src/test/asltest.cxx
+++ src/test/asltest.cxx
@@ -1,13 +1,19 @@
-#include <iostream>
+
+#include "gtest/gtest.h"
 #include "asl.hpp"
 
 using namespace ASL;
 
-int main()
+// int main()
+// {
+//     std::string mykey = random_key();
+//     std::cout << "Generated string: " << mykey << std::endl;
+//     if (mykey.size() == 0) {
+//         return -1;
+//     }
+// }
+TEST(asltest, nonempty)
 {
     std::string mykey = random_key();
-    std::cout << "Generated string: " << mykey << std::endl;
-    if (mykey.size() == 0) {
-        return -1;
-    }
+    EXPECT_NE(0, mykey.size());
 }
