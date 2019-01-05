179a612ef6b2ea2fe0b5603cd7e21a00ea24f01e
Javier Povedano <javier@rti.com>
2019-01-05 10:46:37 +0100

    Add library

---
+++ src/asl.cxx
@@ -0,0 +1,8 @@
+#include "asl.hpp"
+
+std::string ASL::random_key()
+{
+    std::string mystring = "Hello World";
+
+    return mystring;
+}
---
+++ src/asl.hpp
@@ -0,0 +1,5 @@
+#include <string>
+
+namespace ASL {
+    std::string random_key();
+};
